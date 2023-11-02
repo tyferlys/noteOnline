059cee37b77821512d85520a2c139db2147d2e1c
denis klimov <tyferly2003@gmail.com>
2023-11-02 17:08:22 +0400

    малый апдейт верстки

--- resources/views/note/view.blade.php
+++ resources/views/note/view.blade.php
@@ -14,35 +14,38 @@
         @endif
 
         <div class="flex flex-col">
-            <form class="w-5/6 m-auto bg-white rounded-lg p-5" action="{{route("note.update", $note->id)}}" method="post">
-                @csrf
-                @method("patch")
-                <input type="text" name="title" placeholder="Название заметки" value="{{$note->title}}" {{$status==0?"disabled":""}}
-                       class="mb-6 outline-none block w-2/6 m-auto text-center border-b-2 border-gray-800 text-2xl"/>
+            <div class="w-5/6 m-auto bg-white rounded-lg p-5">
+                <form class="min-w-full" action="{{route("note.update", $note->id)}}" method="post">
+                    @csrf
+                    @method("patch")
+                    <input type="text" name="title" placeholder="Название заметки" value="{{$note->title}}" {{$status==0?"disabled":""}}
+                    class="mb-6 outline-none block w-2/6 m-auto text-center border-b-2 border-gray-800 text-2xl"/>
 
-                <div class="w-5/6 m-auto mb-2 text-xl underline">Текст заметки:</div>
-                <textarea {{$status==0?"disabled":""}} name="text" class="min-h-[40vh] resize-y text-lg outline-none p-4 block bg-gray-200 w-5/6 m-auto">{{$note->text}}</textarea>
+                    <div class="w-5/6 m-auto mb-2 text-xl underline">Текст заметки:</div>
+                    <textarea {{$status==0?"disabled":""}} name="text" class="min-h-[40vh] resize-y text-lg outline-none p-4 block bg-gray-200 w-5/6 m-auto">{{$note->text}}</textarea>
+
+                    @if($status == 1)
+                        <div class="flex flex-row justify-center gap-5 mt-4 w-5/6 m-auto">
+                            <input type="submit" value="Сохранить" class="w-2/6 text-lg p-2 bg-gray-800 text-white rounded-lg"/>
+                            <button type="reset" class="w-2/6 text-lg p-2 bg-gray-800 text-white rounded-lg">
+                                Очистить
+                            </button>
+                        </div>
+                    @endif
+                </form>
 
                 @if($status == 1)
-                    <div class="flex flex-row justify-center gap-5 mt-4 w-5/6 m-auto">
-                        <input type="submit" value="Сохранить" class="w-2/6 text-lg p-2 bg-gray-800 text-white rounded-lg"/>
-                        <button type="reset" class="w-2/6 text-lg p-2 bg-gray-800 text-white rounded-lg">
-                            Очистить
-                        </button>
-                    </div>
+                    <form action="{{route("note.delete", $note->id)}}" class="w-5/6 m-auto" method="post">
+                        @csrf
+                        @method("delete")
+                        <input type="submit" class="text-center block w-2/6 m-auto mt-5 text-lg p-2 bg-gray-800 text-white rounded-lg" value="Удалить"/>
+                    </form>
                 @endif
+
                 <div class="w-5/6 text-center m-auto text-md underline mt-5">
                     Дата последнего изменения: {{$note->updated_at}}
                 </div>
-            </form>
-
-            @if($status == 1)
-                <form action="{{route("note.delete", $note->id)}}" class="w-5/6 m-auto" method="post">
-                    @csrf
-                    @method("delete")
-                    <input type="submit" class="text-center block w-2/6 m-auto mt-5 text-lg p-2 bg-gray-800 text-white rounded-lg" value="Удалить"/>
-                </form>
-            @endif
+            </div>
         </div>
     </main>
 @endsection
